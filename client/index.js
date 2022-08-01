3b4d5740675610d0c9514c5041b3e70ee089ee6a
Donna-Sayos <Donnasayos@gmail.com>
2022-08-01 11:50:41 -0400

    yuh

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import '../public/style.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import history from './history';
 import store from './store';
 import App from './App';
