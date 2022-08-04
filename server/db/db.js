6dfa97c3607d3ae88be753bb9f7a0429563ac51e
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-08-04 12:01:30 -0500

    making various changes for deployment

--- server/db/db.js
+++ server/db/db.js
@@ -1,28 +1,44 @@
 const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 
-const dbName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+// const dbName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
-const config = {
-  logging: false,
-};
+// const config = {
+//   logging: false,
+// };
 
-if (process.env.LOGGING === 'true') {
-  delete config.logging;
-}
-//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-// if using Heroku as a deployment service and heroku postgres as db
-if (process.env.DATABASE_URL) {
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-}
+// if (process.env.LOGGING === 'true') {
+//   delete config.logging;
+// }
+// //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+// // if using Heroku as a deployment service and heroku postgres as db
+// if (process.env.DATABASE_URL) {
+//   config.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   };
+// }
+
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+//   config
+// );
+
+const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
+console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
-  config
+  {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
 );
 
 module.exports = db;
