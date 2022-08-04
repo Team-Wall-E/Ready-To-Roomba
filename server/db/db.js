2b66aaee7eae912ccf951cf757dfa4b5df3bc30a
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-08-04 15:17:55 -0500

    fixed local db ssl error

--- server/db/db.js
+++ server/db/db.js
@@ -2,45 +2,29 @@ const Sequelize = require('sequelize');
 const pkg = require('../../package.json');
 // const chalk = require('chalk');
 
-// const dbName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+const dbName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
-// const config = {
-//   logging: false,
-// };
+const config = {
+  logging: false,
+};
 
-// if (process.env.LOGGING === 'true') {
-//   delete config.logging;
-// }
-// //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-// // if using Heroku as a deployment service and heroku postgres as db
-// if (process.env.DATABASE_URL) {
-//   config.dialectOptions = {
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   };
-// }
-
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
-//   config
-// );
-
-const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
-console.log(`Opening database connection to ${dbName}`);
+if (process.env.LOGGING === 'true') {
+  delete config.logging;
+}
+//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+// if using Heroku as a deployment service and heroku postgres as db
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
-  {
-    logging: false,
-    dialectOptions: {
-      //ssl: process.env.DATABASE_URL ? true : false
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
+  config
 );
 
 module.exports = db;
