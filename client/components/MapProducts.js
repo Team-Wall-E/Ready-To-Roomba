fe43fb43bb90ec769a2302bd874f56f1185da184
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-08-05 08:34:35 -0500

    removed bootstrap classnames to start again

--- client/components/MapProducts.js
+++ client/components/MapProducts.js
@@ -5,31 +5,16 @@ export default function MapProducts(props) {
   console.log(props.products);
   return props.products.map((product) => {
     return (
-      <div className="col" key={product.id}>
-        <div className="card shadow-sm">
-          <img
-            className="card-img-top"
-            src={product.imageUrl}
-            alt="image of product"
-          />
-          <div className="card-body">
-            <h4 className="card-title">
+      <div key={product.id}>
+        <div>
+          <img src={product.imageUrl} alt="image of product" />
+          <div>
+            <h4>
               <Link to={`/products/${product.id}`}>{product.productName}</Link>
             </h4>
 
-            <address className="card-subtitle">{product.price}</address>
-            <p className="card-text">{product.description}</p>
-            <div className="d-flex justify-content-between align-items-center bottom-buttons">
-              <div className="btn-group">
-                <Link
-                  type="button"
-                  className="btn btn-sm btn-outline-secondary"
-                  to={`/products/${product.id}`}
-                >
-                  View
-                </Link>
-              </div>
-            </div>
+            <address>{product.price}</address>
+            <p>{product.description}</p>
           </div>
         </div>
       </div>
