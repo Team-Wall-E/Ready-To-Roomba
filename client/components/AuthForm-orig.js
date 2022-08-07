987302f747d1a78b1b9c34b9e3d09a0934428680
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-08-07 12:56:14 -0500

    styled navbar, added logo, started styling auth form

--- client/components/AuthForm-orig.js
+++ client/components/AuthForm-orig.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { authenticate } from '../store';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 /**
  * COMPONENT
@@ -11,34 +9,38 @@ const AuthForm = (props) => {
   const { name, displayName, handleSubmit, error } = props;
   // TODO: add ternary to not show first and last name on login form
   return (
-    <Form className="form-signin w-100 m-auto needs-validation">
-      <Form.Group className="mb-3" controlId="formFirstName">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control name="firstName" type="text" placeholder="First Name" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formLastName">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control name="lastName" type="text" placeholder="Last Name" />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" required />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" required />
-      </Form.Group>
-
-      <Button variant="dark" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="firstName">
+            <small>First Name</small>
+          </label>
+          <input name="firstName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="laststName">
+            <small>Last Name</small>
+          </label>
+          <input name="lastName" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+    </div>
   );
 };
 
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,31 +2,49 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <h1>Welcome!</h1>
-    <nav>
+  <nav className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 black-bg">
+    {/* logo */}
+    <Image
+      src="logo.png"
+      href="/home"
+      className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-black text-decoration-none"
+    ></Image>
+    {/* middle links */}
+    <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+      <li>
+        <a href="/home" className="nav-link px-2 link-secondary">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/products" className="nav-link px-2 link-light">
+          Products
+        </a>
+      </li>
+    </ul>
+    {/* far right icons/buttons */}
+
+    <div className="col-md-3 text-end">
       {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/products">Products</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
+        <Button variant="light" href="/logout">
+          Logout
+        </Button>
       ) : (
         <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/products">Products</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+          <Button variant="light" href="/login">
+            Login
+          </Button>
+          <Button variant="light" href="/signup">
+            Sign-up
+          </Button>
         </div>
       )}
-    </nav>
-    <hr />
-  </div>
+    </div>
+  </nav>
 );
 
 /**
