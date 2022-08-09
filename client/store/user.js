45f0ee5293cfda9794c4dd4f683399b720a5d464
Selina <gooblersell@gmail.com>
2022-08-09 15:11:42 -0400

    user thunks added, not complete

---
+++ client/store/user.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+
+const SET_USER = 'SET_USER';
+const CREATE_USER = 'CREATE_USER';
+const DELETE_USER = 'DELETE_USER';
+
+export const setUser = (user) => ({
+  type: SET_USER,
+  user,
+});
+
+const createUser = (user) => {
+  return {
+    type: CREATE_USER,
+    user,
+  };
+};
+
+const deleteUser = (user) => {
+  return {
+    type: DELETE_USER,
+    user,
+  };
+};
+
+const updateUser = (user) => {
+  return {
+     type: UPDATE_USER,
+     user,
+  };
+};
+
+export const fetchUser = () => async (dispatch) => {
+  const userResponse = await axios.get('/api/user');
+  dispatch(setUser(userResponse.data));
+};
+
+const TOKEN = 'token';
+
+export const createUserThunk = (user, history) => {
+  return async (dispatch) => {
+    try {
+      const token = window.localStorage.getItem(TOKEN);
+      const response = await axios.post(`/api/user`, user, {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(createUser(response.data));
+      history.push('/user');
+    } catch (err) {
+      console.log(err.response);
+    }
+  };
+};
+
+export const deleteUserThunk = (id, history) => {
+  return async (dispatch) => {
+    try {
+      const token = window.localStorage.getItem(TOKEN);
+      const { data: user } = await axios.delete(`/api/user/${id}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(deleteUser(user));
+      history.push('/user');
+    } catch (err) {
+      console.log(err.response);
+    }
+  };
+};
+
+export const updateUserThunk = (id, user) => {
+  const token = window.localStorage.getItem(TOKEN);
+  return async (dispatch) => {
+     try {
+        const response = await axios.put(`/api/users/${id}`, user, {
+           headers: {
+              authorization: token,
+           },
+        });
+        dispatch(updateUser(response.data));
+     } catch (err) {
+        console.log(err.response);
+     }
+  };
+};
+
+export default function userReducer(user = [], action) {
+  switch (action.type) {
+    case SET_USER:
+      return action.user;
+    case CREATE_USER:
+      return [...user, action.user];
+    case DELETE_USER:
+      return user.filter((user) => user.id !== action.user.id);
+    case UPDATE_USER:
+      return { ...user, ...action.user };
+    default:
+      return user;
+  }
+}
--- server/api/index.js
+++ server/api/index.js
@@ -3,11 +3,15 @@ module.exports = router;
 
 router.use('/users', require('./users'));
 router.use('/products', require('./products'));
-router.use('/cart', require('./cart'));
 router.use('/reviews', require('./reviews'));
+router.use('/cart', require('./cart'));
+
+//TODO: jeremy/alec assistance requested
+// router.use('/cart', require('./users/:id/cart'));
+// router.use('/orders', require('./users/:id/orders'));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
