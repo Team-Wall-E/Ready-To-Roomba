18193bc3d692aad8731d81366411fe12d4fdb0bd
Selina <gooblersell@gmail.com>
2022-08-14 16:26:51 -0400

    stripe adjusted && signup isAdmin adjusted

--- client/components/Stripe.js
+++ client/components/Stripe.js
@@ -1,37 +1,38 @@
-import React, { Fragment } from "react";
-import StripeCheckout from "react-stripe-checkout";
-import axios from "axios";
+import React, { Fragment } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeBtn = () => {
-    console.log(StripeCheckout)
-  const publishableKey = "pk_test_51LUbVTEXNlVtLEphjQMCFjrln5Vt91r2BE6koJHyUaDp1qTSnaC8LP8OJ4UnpJBCGPTYTqP5ZtHnzOEYC54JhT0600c3UdcJCe";
-   
-  const onToken = token => {
+  console.log(StripeCheckout);
+  const publishableKey =
+    'pk_test_51LUbVTEXNlVtLEphjQMCFjrln5Vt91r2BE6koJHyUaDp1qTSnaC8LP8OJ4UnpJBCGPTYTqP5ZtHnzOEYC54JhT0600c3UdcJCe';
+
+  const onToken = (token) => {
     const body = {
       amount: 999,
-      token: token
-  };
-  axios
-      .post("/api/cart/stripe", body)
-      .then(response => {
+      token: token,
+    };
+    axios
+      .post('/api/cart/stripe', body)
+      .then((response) => {
         console.log(response);
-        alert("Payment Success");
+        alert('Payment successful');
       })
-      .catch(error => {
-        console.log("Payment Error: ", error);
-        alert("You broke ass b*tch");
+      .catch((error) => {
+        console.log('Payment Error: ', error);
+        alert('Payment declined');
       });
   };
   return (
     <StripeCheckout
-      label="Payment" //Component button text
-      name="Ready To Roomba" //Modal Header
-      description="Let us take your money daddy 😏"
-      panelLabel="Pay us" //Submit button in modal
-    //   amount={999} //Amount in cents $9.99
+      label='Confirm Payment' //Component button text
+      name='Ready To Roomba' //Modal Header
+      description='Thank you for your support!'
+      panelLabel='Confirm' //Submit button in modal
+      //   amount={999} //Amount in cents $9.99
       token={onToken}
       stripeKey={publishableKey}
-      image="https://scalebranding.com/wp-content/uploads/2021/07/Star-game-robot-logo.jpg" //Pop-in header image
+      image='https://scalebranding.com/wp-content/uploads/2021/07/Star-game-robot-logo.jpg' //Pop-in header image
       billingAddress={false}
     />
   );
