5e1cc2dd26dcc7b27d116a552f54d07f6ae4f78e
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2022-12-07 10:22:52 -0600

    updated db url to bit.io

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,13 @@
-const Sequelize = require("sequelize");
-const pkg = require("../../package.json");
+const Sequelize = require('sequelize');
+const pkg = require('../../package.json');
 
-const dbName = pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
+// const dbName = pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 
 const config = {
   logging: false,
 };
 
-if (process.env.LOGGING === "true") {
+if (process.env.LOGGING === 'true') {
   delete config.logging;
 }
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
@@ -22,7 +22,9 @@ if (process.env.DATABASE_URL) {
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+  process.env.DATABASE_URL ||
+    `postgresql://vljones133:v2_3wWAu_xSTKRtqxjWrzK9dGpYbmkfR@db.bit.io/vljones133/grace-shopper?ssl=true`,
+  // process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
   config
 );
 
