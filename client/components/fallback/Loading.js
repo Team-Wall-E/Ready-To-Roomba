c230e3c8397190414224bf7a44a2cd072137f6cc
Valerie Jones <4821024+vljones133@users.noreply.github.com>
2023-01-12 11:19:15 -0600

    added react import to Loading.js

--- client/components/fallback/Loading.js
+++ client/components/fallback/Loading.js
@@ -1,9 +1,10 @@
-import Spinner from "react-bootstrap/Spinner";
+import React from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function Loading() {
   return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
+    <Spinner animation='border' role='status'>
+      <span className='visually-hidden'>Loading...</span>
     </Spinner>
   );
 }
